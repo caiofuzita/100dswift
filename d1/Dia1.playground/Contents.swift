fa748b617c7fcb87242ed52d77da44963d09f9c8
caiofuzita <caiofuzita@hotmail.com>
2025-07-28 21:14:50 -0300

    Dia 1

---
+++ d1/Dia1.playground/Contents.swift
@@ -0,0 +1,39 @@
+import UIKit
+
+// 1. Variables
+var str = "Hello, playground"
+
+str = "Goodbye"
+
+// 2. String and integers
+var age = 38
+
+var popultion = 8_000_000
+
+// 3. Multi-line strings
+var str1 = """
+This goes
+over multiple
+lines
+"""
+
+// 4. Doubles and booleans
+var pi = 3.1415
+var awesome = true
+
+// 5. String interpolation
+var score = 85
+var str3 = "Your score was \(score)"
+
+
+var results = "The test results are here: \(str3)"
+
+
+// 5. Constants
+let gravity = 9.8
+
+// 6. Annotations
+let name: String = "Vectra"
+let year: Int = 2006
+let engine: Double = 2.0
+let bebeMuito: Bool = true
